790e297592e34d325cbf2b5ad8d108e79db69fde
Yuki-Kono213 <marushige9685@gmail.com>
2021-09-01 13:57:57 +0900

    change https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        if(\App::enviroment(['production'])){
+        if(\App::environment(['production']) || \App::environment(['develop']))
+        {
             \URL::forceScheme('https');
         }
     }
